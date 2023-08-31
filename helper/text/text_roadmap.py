2aaf96a1bbc05bb258909280150e25bc31140f2a
Sneriko <erik.lenas@gmail.com>
2023-08-31 10:59:33 +0200

    roadmap

--- helper/text/text_roadmap.py
+++ helper/text/text_roadmap.py
@@ -2,7 +2,12 @@ class TextRoadmap:
     roadmap = """
 
     ## Roadmap
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+    * Continually retrain and update both segmentation och text-recognition models as more training-data becomes available.
+    * Train a TrOCR model specialized on swedish historical handwritten text that is initialized with an historical BERT-model trained here at the Swedish National Archives.
+    * An open-source HTR-project where it is easy to implement pipelines such as the one shown in the demo, but that is highly modularized, so you can implement different segmentation strategies, implement models trained with a variety of frameworks, and that offers effective ways to evaluate entire pipelines and compare them with each other. Use-cases should not be restricted to running text, but should comprise all types of handwritten archives.
+    * Do large scale HTR on handwritten historical archives that are of interest to researchers within the humanities. 
+   
     """
 
     notebook = """
