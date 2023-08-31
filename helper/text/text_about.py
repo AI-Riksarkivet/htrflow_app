004cf42397cdce11c7900c147c0dfbc7a5c0f2e5
Borg93 <48671678+Borg93@users.noreply.github.com>
2023-08-31 13:58:13 +0200

    Update text_about.py

--- helper/text/text_about.py
+++ helper/text/text_about.py
@@ -19,7 +19,7 @@ class TextAbout:
     By training on only binarized images and by binarizing images before running them through the pipeline, we take the target domain closer to the training domain, and ruduce negative effects of background variation, background noise etc., on the final results. 
     The pipeline implements a simple adaptive thresholding algorithm for binarization.
     <figure>
-    <img src="https://raw.githubusercontent.com/Borg93/htr_gradio_file_placeholder/main/roadmap_image_2.png" alt="HTR_tool" style="width:70%; display: block; margin-left: auto; margin-right:auto;" >
+    <img src="https://github.com/Borg93/htr_gradio_file_placeholder/blob/main/app_project_bin.png?raw=true" alt="HTR_tool" style="width:70%; display: block; margin-left: auto; margin-right:auto;" >
     </figure> 
 
 """
@@ -29,7 +29,7 @@ class TextAbout:
     The segmentation model utilized in this process predicts both bounding boxes and masks. Although the model has the capability to predict both, only the masks are utilized for the segmentation tasks of lines and regions.
     An essential post-processing step involves checking for regions that are contained within other regions. During this step, only the containing region is retained, while the contained region is discarded. This ensures that the final segmented text-regions are accurate and devoid of overlapping or redundant areas.
     <figure>
-    <img src="https://raw.githubusercontent.com/Borg93/htr_gradio_file_placeholder/main/roadmap_image_2.png" alt="HTR_tool" style="width:70%; display: block; margin-left: auto; margin-right:auto;" >
+    <img src="https://github.com/Borg93/htr_gradio_file_placeholder/blob/main/app_project_region.png?raw=true" alt="HTR_tool" style="width:70%; display: block; margin-left: auto; margin-right:auto;" >
     </figure> 
 """
     text_line_segmentation = """
@@ -38,7 +38,7 @@ class TextAbout:
     This is also an RTMDet model that's trained on extracting text-lines from cropped text-regions within an image. 
     The same post-processing on the instance segmentation masks is done here as in the text-region segmentation step.
     <figure>
-    <img src="https://raw.githubusercontent.com/Borg93/htr_gradio_file_placeholder/main/roadmap_image_2.png" alt="HTR_tool" style="width:70%; display: block; margin-left: auto; margin-right:auto;" >
+    <img src="https://github.com/Borg93/htr_gradio_file_placeholder/blob/main/app_project_line.png?raw=true" alt="HTR_tool" style="width:70%; display: block; margin-left: auto; margin-right:auto;" >
     </figure> 
 """
     text_htr = """
@@ -47,20 +47,24 @@ class TextAbout:
     For the text-recognition a SATRN model was trained with mmocr on approximately one million handwritten text-line images ranging from 1600 to 1900. 
     It was trained on a wide variety of archival material to make it generalize as well as possible. See below for detailed evaluation results, and also some finetuning experiments.
     <figure>
-    <img src="https://raw.githubusercontent.com/Borg93/htr_gradio_file_placeholder/main/roadmap_image_2.png" alt="HTR_tool" style="width:70%; display: block; margin-left: auto; margin-right:auto;" >
+    <img src="https://github.com/Borg93/htr_gradio_file_placeholder/blob/main/app_project_htr.png?raw=true" alt="HTR_tool" style="width:70%; display: block; margin-left: auto; margin-right:auto;" >
     </figure> 
     """
 
     text_data = """
-    ## The Data
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
+    
+    For a glimpse into the kind of data we're working with, you can explore our sample test data on Hugging Face:
+    
+    [![Sample Test Data](https://img.shields.io/badge/Sample%20Test%20Data-View-blue.svg)](https://huggingface.co/datasets/Riksarkivet/test_images_demo)
+    
+    **Note**: This is just a sample. The complete training dataset will be released in the future.
     """
 
     text_models = """
     ## The Models
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
+    For detailed information about all the models used in this project, please refer to the model cards available on Hugging Face:
+    
+    [![Model Cards](https://img.shields.io/badge/Model%20Cards-View-blue.svg)](https://huggingface.co/Riksarkivet/HTR_pipeline_models)
     """
 
 
