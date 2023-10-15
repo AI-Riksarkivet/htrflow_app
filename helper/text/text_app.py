83d2c6a81ced7e9cf26547dd04b0e1796bd3abeb
borg93 <gabriel.borg93@gmail.com>
2023-10-15 20:55:37 +0200

    test new scheduler

--- helper/text/text_app.py
+++ helper/text/text_app.py
@@ -1,11 +1,15 @@
 class TextApp:
     title_markdown = """
 
-    <img src="https://raw.githubusercontent.com/Borg93/Riksarkivet_docs/main/docs/assets/fav-removebg-preview.png" width="4%" align="right" margin-right="100" />
 
-    <h1><center> Handwritten Text Recognition Tool </center></h1>
+    <h1><center> HTRflow - Demo </center></h1>
 
     <h3><center> Swedish National Archives - Riksarkivet </center></h3>"""
+
+    title_markdown_img = """
+    <img src="https://raw.githubusercontent.com/Borg93/Riksarkivet_docs/main/docs/assets/fav-removebg-preview.png" width="13%" align="right" margin-right="100" />
+
+    """
 
 
 if __name__ == "__main__":
