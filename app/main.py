6829cf5847f47eafec1000f0341f1fca8ff256cd
viklofg <viktoria.lofgren@riksarkivet.se>
2025-01-24 13:14:16 +0100

    Reset server port

--- app/main.py
+++ app/main.py
@@ -130,7 +130,7 @@ demo.queue()
 if __name__ == "__main__":
     demo.launch(
         server_name="0.0.0.0",
-        server_port=7862,
+        server_port=7861,
         enable_monitoring=True,
         # show_error=True,
     )
