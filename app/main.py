8d7c1dded2d690781dbdbb2433600aadd1854495
viklofg <viktoria.lofgren@riksarkivet.se>
2025-01-28 10:44:12 +0100

    Update port

--- app/main.py
+++ app/main.py
@@ -136,7 +136,7 @@ demo.queue()
 if __name__ == "__main__":
     demo.launch(
         server_name="0.0.0.0",
-        server_port=7861,
+        server_port=7860,
         enable_monitoring=True,
         # show_error=True,
     )
